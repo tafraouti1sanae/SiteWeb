9dd9d7f564471f36a917a44c155760c90bb55013
Tafraouti Sanae <tafraoutist@gmail.com>
2025-04-10 19:15:50 +0100

    Ajout des fichiers HTML, CSS, JS et PHP

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h2>Page PHP Fonctionnelle</h2>";
+    echo "<p>Serveur PHP configuré avec succès.</p>";
+?>
---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("JavaScript fonctionne !");
+document.querySelector("h1").addEventListener("click", () => {
+    alert("Vous avez cliqué sur le titre !");
+});
+
+
+
+
+
+
+
+
+
+
